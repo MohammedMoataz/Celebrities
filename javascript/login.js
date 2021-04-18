c6053365cd957ac79019ca7e49741ca100e14870
MohammedMoataz <81831838+MohammedMoataz@users.noreply.github.com>
2021-04-19 00:21:26 +0200

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -7,32 +7,6 @@ var page = document.getElementById('login'),
     eye_1 = document.getElementById('eye1'),
     eye_2 = document.getElementById('eye2');
 
-// link[6].onmouseover = function() {
-//     'use strick';
-//     link[6].classList.add('hover');
-// };
-// link[6].onmouseout = function() {
-//     'use strick';
-//     link[6].classList.remove('hover');
-// };
-
-// link[8].onmouseover = function() {
-//     'use strick';
-//     link[8].classList.add('hover');
-// };
-// link[8].onmouseout = function() {
-//     'use strick';
-//     link[8].classList.remove('hover');
-// };
-
-// login.onmouseover = function() {
-//     'use strick';
-//     login.classList.add('button3d');
-// };
-// login.onmouseout = function() {
-//     'use strick';
-//     login.classList.remove('button3d');
-// };
 
 window.onload = function() {
     'use strick';
@@ -45,19 +19,8 @@ login.onclick = function() {
     console.log(userName.value);
     console.log(password.value);
     console.log(password.value.length);
-
-    // if( password.value.length < 6) {
-        // fw.classList.add('error');
-
-        // alert("Your password is week");
-
-        // fw.onmouseover = function() {
-        //     fw.classList.remove('error');
-        // };
-    // }
-    // else {
-        window.location.href = "../index/home.html";
-    // }
+    
+    window.location.href = "../index/home.html";
 };
 
 switcher.onclick = function() {
